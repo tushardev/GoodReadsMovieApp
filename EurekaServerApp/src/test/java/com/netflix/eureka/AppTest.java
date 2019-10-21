f1c0839fecbcfd97f41edf6cc20a3a6940a68356
tushardev <kadale.tushar@gmail.com>
2019-10-21 07:07:01 +0530

    Eureka Server App

---
+++ EurekaServerApp/src/test/java/com/netflix/eureka/AppTest.java
@@ -0,0 +1,20 @@
+package com.netflix.eureka;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple EurekaServerApp.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
